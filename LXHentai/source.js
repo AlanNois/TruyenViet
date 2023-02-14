e3c07ddb546bff46db69c8b46ecf2d745595fcb4
AlanNois <none@iesschool.edu.vn>
2023-02-14 13:38:15 +0700

    Updates

--- LXHentai/source.js
+++ LXHentai/source.js
@@ -2503,18 +2503,18 @@ class LXHentai extends paperback_extensions_common_1.Source {
             const $ = this.cheerio.load(response.data);
             const chapters = [];
             var i = 0;
-            for (const obj of $(".overflow-y-auto.overflow-x-hidden").toArray().reverse()) {
+            for (const obj of $(".overflow-y-auto.overflow-x-hidden > a").toArray().reverse()) {
                 i++;
-                let time = $(`a:nth-child(${i}) > li > div.hidden > span.timeago`, obj).text();
                 chapters.push(createChapter({
-                    id: 'https://lxmanga.net' + $(`a:nth-child(${i})`, obj).attr('href'),
+                    id: 'https://lxmanga.net' + $(obj).attr('href'),
                     chapNum: i,
-                    name: $(`a:nth-child(${i}) > li > div > span.text-ellipsis`, obj).text(),
+                    name: $(`li > div > span.text-ellipsis`, obj).text(),
                     mangaId: mangaId,
                     langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
-                    time: new Date(time)
+                    time: null
                 }));
             }
+            // console.log(chapters);
             return chapters;
         });
     }
@@ -2632,7 +2632,7 @@ class LXHentai extends paperback_extensions_common_1.Source {
                     url = `https://lxmanga.net/story/index.php?hot&p=${page}`;
                     break;
                 case "new_updated":
-                    url = `https://lxmanga.net/story/index.php?p=${page}`;
+                    url = `https://lxmanga.net/tim-kiem?sort=-updated_at&filter%5Bstatus%5D=2%2C1&page=${page}`;
                     break;
                 default:
                     return Promise.resolve(createPagedResults({ results: [] }));
@@ -2706,7 +2706,7 @@ exports.LXHentai = LXHentai;
 },{"./LXHentaiParser":52,"buffer":2,"paperback-extensions-common":7}],52:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isLastPage = exports.parseTags = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
+exports.convertTime = exports.isLastPage = exports.parseTags = exports.parseViewMore = exports.parseSearch = exports.generateSearch = void 0;
 exports.generateSearch = (query) => {
     var _a;
     let keyword = (_a = query.title) !== null && _a !== void 0 ? _a : "";
@@ -2798,9 +2798,46 @@ exports.isLastPage = ($) => {
         isLast = true;
     return isLast;
 };
-// const decodeHTMLEntity = (str: string): string => {
-//     return entities.decodeHTML(str);
-// }
+function convertTime(timeAgo) {
+    var _a;
+    let time;
+    let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
+    trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
+    if (timeAgo.includes('giây') || timeAgo.includes('secs')) {
+        time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
+    }
+    else if (timeAgo.includes('phút')) {
+        time = new Date(Date.now() - trimmed * 60000);
+    }
+    else if (timeAgo.includes('giờ')) {
+        time = new Date(Date.now() - trimmed * 3600000);
+    }
+    else if (timeAgo.includes('ngày')) {
+        time = new Date(Date.now() - trimmed * 86400000);
+    }
+    else if (timeAgo.includes('ngày')) {
+        time = new Date(Date.now() - trimmed * 86400000);
+    }
+    else if (timeAgo.includes('năm')) {
+        time = new Date(Date.now() - trimmed * 31556952000);
+    }
+    else {
+        if (timeAgo.includes(":")) {
+            let split = timeAgo.split(' ');
+            let H = split[0]; //vd => 21:08
+            let D = split[1]; //vd => 25/08 
+            let fixD = D.split('/');
+            let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
+            time = new Date(finalD + ' ' + H);
+        }
+        else {
+            let split = timeAgo.split('/'); //vd => 05/12/18
+            time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
+        }
+    }
+    return time;
+}
+exports.convertTime = convertTime;
 
 },{}]},{},[51])(51)
 });
