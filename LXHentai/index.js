0a803abdc9d14d850f7bbd339b7243eaddc71eb8
AlanNois <none@iesschool.edu.vn>
2023-02-24 14:46:28 +0700

    Updates

--- LXHentai/index.js
+++ LXHentai/index.js
@@ -2659,7 +2659,7 @@ class LXHentai extends paperback_extensions_common_1.Source {
             let page = (_a = metadata === null || metadata === void 0 ? void 0 : metadata.page) !== null && _a !== void 0 ? _a : 1;
             const tags = (_c = (_b = query.includedTags) === null || _b === void 0 ? void 0 : _b.map(tag => tag.id)) !== null && _c !== void 0 ? _c : [];
             const request = createRequestObject({
-                url: query.title ? `https://lxmanga.net/tim-kiem?sort=-updated_at&filter%5Bname%5D=${encodeURI(query.title)}&filter%5Bstatus%5D=2%2C1&page=${page}` : `${tags[0]}&p=${page}`,
+                url: query.title ? `https://lxmanga.net/tim-kiem?sort=-updated_at&filter%5Bname%5D=${encodeURI(query.title)}&filter%5Bstatus%5D=2%2C1&page=${page}` : `${tags[0]}&page=${page}`,
                 method: "GET",
             });
             const data = yield this.requestManager.schedule(request, 1);
