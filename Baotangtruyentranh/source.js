265e4c83b7e136dd2758d219731012d17de5dfee
AlanNois <none@iesschool.edu.vn>
2023-03-08 19:01:18 +0700

    Updates

--- Baotangtruyentranh/source.js
+++ Baotangtruyentranh/source.js
@@ -635,48 +635,6 @@ class Baotangtruyentranh extends paperback_extensions_common_1.Source {
             }
         });
     }
-    convertTime(timeAgo) {
-        var _a;
-        let time;
-        let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
-        trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
-        if (timeAgo.includes('giây')) {
-            time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
-        }
-        else if (timeAgo.includes('phút')) {
-            time = new Date(Date.now() - trimmed * 60000);
-        }
-        else if (timeAgo.includes('giờ')) {
-            time = new Date(Date.now() - trimmed * 3600000);
-        }
-        else if (timeAgo.includes('ngày')) {
-            time = new Date(Date.now() - trimmed * 86400000);
-        }
-        else if (timeAgo.includes('tuần')) {
-            time = new Date(Date.now() - trimmed * 86400000 * 7);
-        }
-        else if (timeAgo.includes('tháng')) {
-            time = new Date(Date.now() - trimmed * 86400000 * 7 * 4);
-        }
-        else if (timeAgo.includes('năm')) {
-            time = new Date(Date.now() - trimmed * 86400000 * 7 * 4 * 12);
-        }
-        else {
-            if (timeAgo.includes(":")) {
-                let split = timeAgo.split(' ');
-                let H = split[0]; //vd => 21:08
-                let D = split[1]; //vd => 25/08 
-                let fixD = D.split('/');
-                let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
-                time = new Date(finalD + ' ' + H);
-            }
-            else {
-                let split = timeAgo.split('/'); //vd => 05/12/18
-                time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
-            }
-        }
-        return time;
-    }
     getMangaShareUrl(mangaId) { return (mangaId); }
     ;
     getMangaDetails(mangaId) {
@@ -714,7 +672,6 @@ class Baotangtruyentranh extends paperback_extensions_common_1.Source {
         });
     }
     getChapters(mangaId) {
-        var _a;
         return __awaiter(this, void 0, void 0, function* () {
             let StoryID = mangaId.split('-').pop();
             const request = createRequestObject({
@@ -724,31 +681,27 @@ class Baotangtruyentranh extends paperback_extensions_common_1.Source {
             });
             let data = yield this.requestManager.schedule(request, 1);
             let $ = this.cheerio.load(data.data);
-            const chapters = [];
-            for (let obj of $('ul .row:not(.heading)').toArray()) {
-                let ids = $('a', obj).first().attr('href');
-                let id = ids.replace(ids.match(/chapter-\d+/), mangaId.split('/')[mangaId.split('/').length - 1].split('-').slice(0, -1).join('-'));
-                let chapNum = parseFloat((_a = $('a', obj).first().text()) === null || _a === void 0 ? void 0 : _a.split(' ')[1]);
-                let name = ($('a', obj).first().text().trim() === ('Chapter ' + chapNum.toString())) ? $('a', obj).first().text().trim() : '';
-                // if ($('.coin-unlock', obj).attr('title')) {
-                // name = 'LOCKED (' + $('.coin-unlock', obj).attr('title') + ')';
-                // }
-                // let time = $('.col-xs-4', obj).text().trim();
-                // let chapNum = 1
-                // let id = $('a', $('ul .row:not(.heading)').toArray()[0]).first().attr('href')
-                // let name = $('a', $('ul .row:not(.heading)').toArray()[0]).first().text().trim();
-                let time = '';
-                chapters.push(createChapter({
-                    id,
-                    chapNum: chapNum,
-                    name,
-                    mangaId: mangaId,
-                    langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
-                    time: this.convertTime(BaotangtruyentranhParser_1.decodeHTMLEntity(time))
-                }));
-            }
-            console.log(chapters);
-            return chapters;
+            // const chapters: Chapter[] = [];
+            // for (let obj of $('ul .row:not(.heading)').toArray()) {
+            //     let ids = $('a', obj).first().attr('href');
+            //     let id = ids.replace(ids.match(/chapter-\d+/), mangaId.split('/')[mangaId.split('/').length - 1].split('-').slice(0, -1).join('-'));
+            //     let chapNum = parseFloat($('a', obj).first().text()?.split(' ')[1]);
+            //     let name = ($('a', obj).first().text().trim() === ('Chapter ' + chapNum.toString())) ? $('a', obj).first().text().trim() : '';
+            //     if ($('.coin-unlock', obj).attr('title')) {
+            //         name = 'LOCKED (' + $('.coin-unlock', obj).attr('title') + ')';
+            //     }
+            //     let time = $('.col-xs-4', obj).text().trim();
+            //     chapters.push(createChapter(<Chapter>{
+            //         id,
+            //         chapNum: chapNum,
+            //         name,
+            //         mangaId: mangaId,
+            //         langCode: LanguageCode.VIETNAMESE,
+            //         time: this.convertTime(decodeHTMLEntity(time))
+            //     }));
+            // }
+            // console.log(chapters)
+            return BaotangtruyentranhParser_1.parseChapterList($, mangaId);
         });
     }
     getChapterDetails(mangaId, chapterId) {
@@ -1018,7 +971,8 @@ exports.Baotangtruyentranh = Baotangtruyentranh;
 },{"./BaotangtruyentranhParser":57,"paperback-extensions-common":12}],57:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.decodeHTMLEntity = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = exports.capitalizeFirstLetter = void 0;
+exports.parseChapterList = exports.decodeHTMLEntity = exports.isLastPage = exports.parseViewMore = exports.parseSearch = exports.generateSearch = exports.capitalizeFirstLetter = void 0;
+const paperback_extensions_common_1 = require("paperback-extensions-common");
 const entities = require("entities"); //Import package for decoding HTML entities
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -1081,6 +1035,71 @@ exports.isLastPage = ($) => {
 exports.decodeHTMLEntity = (str) => {
     return entities.decodeHTML(str);
 };
+function convertTime(timeAgo) {
+    var _a;
+    let time;
+    let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
+    trimmed = (trimmed == 0 && timeAgo.includes('a')) ? 1 : trimmed;
+    if (timeAgo.includes('giây')) {
+        time = new Date(Date.now() - trimmed * 1000); // => mili giây (1000 ms = 1s)
+    }
+    else if (timeAgo.includes('phút')) {
+        time = new Date(Date.now() - trimmed * 60000);
+    }
+    else if (timeAgo.includes('giờ')) {
+        time = new Date(Date.now() - trimmed * 3600000);
+    }
+    else if (timeAgo.includes('ngày')) {
+        time = new Date(Date.now() - trimmed * 86400000);
+    }
+    else if (timeAgo.includes('tuần')) {
+        time = new Date(Date.now() - trimmed * 86400000 * 7);
+    }
+    else if (timeAgo.includes('tháng')) {
+        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4);
+    }
+    else if (timeAgo.includes('năm')) {
+        time = new Date(Date.now() - trimmed * 86400000 * 7 * 4 * 12);
+    }
+    else {
+        if (timeAgo.includes(":")) {
+            let split = timeAgo.split(' ');
+            let H = split[0]; //vd => 21:08
+            let D = split[1]; //vd => 25/08 
+            let fixD = D.split('/');
+            let finalD = fixD[1] + '/' + fixD[0] + '/' + new Date().getFullYear();
+            time = new Date(finalD + ' ' + H);
+        }
+        else {
+            let split = timeAgo.split('/'); //vd => 05/12/18
+            time = new Date(split[1] + '/' + split[0] + '/' + '20' + split[2]);
+        }
+    }
+    return time;
+}
+exports.parseChapterList = ($, mangaId) => {
+    var _a;
+    const chapters = [];
+    for (let obj of $('ul .row:not(.heading)').toArray()) {
+        let ids = $('a', obj).first().attr('href');
+        let id = ids.replace(ids.match(/chapter-\d+/), mangaId.split('/')[mangaId.split('/').length - 1].split('-').slice(0, -1).join('-'));
+        let chapNum = parseFloat((_a = $('a', obj).first().text()) === null || _a === void 0 ? void 0 : _a.split(' ')[1]);
+        let name = ($('a', obj).first().text().trim() === ('Chapter ' + chapNum.toString())) ? $('a', obj).first().text().trim() : '';
+        if ($('.coin-unlock', obj).attr('title')) {
+            name = 'LOCKED (' + $('.coin-unlock', obj).attr('title') + ')';
+        }
+        let time = $('.col-xs-4', obj).text().trim();
+        chapters.push(createChapter({
+            id,
+            chapNum: chapNum,
+            name,
+            mangaId: mangaId,
+            langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
+            time: convertTime(exports.decodeHTMLEntity(time))
+        }));
+    }
+    return chapters;
+};
 
-},{"entities":1}]},{},[56])(56)
+},{"entities":1,"paperback-extensions-common":12}]},{},[56])(56)
 });
