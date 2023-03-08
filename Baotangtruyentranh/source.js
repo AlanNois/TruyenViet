18ae0fb20caebe3d8ebb2de85c93bdefdf96e905
AlanNois <none@iesschool.edu.vn>
2023-03-08 12:03:34 +0700

    Updates

--- Baotangtruyentranh/source.js
+++ Baotangtruyentranh/source.js
@@ -716,31 +716,30 @@ class Baotangtruyentranh extends paperback_extensions_common_1.Source {
     getChapters(mangaId) {
         var _a;
         return __awaiter(this, void 0, void 0, function* () {
+            let mangaID = mangaId;
             let StoryID = mangaId.split('-').pop();
             // console.log(StoryID);
             const request = createRequestObject({
                 url: 'https://baotangtruyennet.com/Story/ListChapterByStoryID',
-                method,
+                method: 'POST',
                 data: { StoryID: StoryID }
             });
             let data = yield this.requestManager.schedule(request, 1);
             let $ = this.cheerio.load(data.data);
             const chapters = [];
             for (const obj of $('nav .row:not(.heading)').toArray()) {
-                // let id = $('a', obj).first().attr('href');
                 let id = $('a', obj).first().attr('href');
                 let chapNum = parseFloat((_a = $('a', obj).first().text()) === null || _a === void 0 ? void 0 : _a.split(' ')[1]);
-                // let name = ($('a', obj).first().text().trim() === ('Chapter ' + chapNum.toString())) ? $('a', obj).first().text().trim() : '';
-                let name = 'a';
-                // if ($('.coin-unlock', obj).attr('title')) {
-                //     name = 'LOCKED (' + $('.coin-unlock', obj).attr('title') + ')';
-                // }
+                let name = ($('a', obj).first().text().trim() === ('Chapter ' + chapNum.toString())) ? $('a', obj).first().text().trim() : '';
+                if ($('.coin-unlock', obj).attr('title')) {
+                    name = 'LOCKED (' + $('.coin-unlock', obj).attr('title') + ')';
+                }
                 let time = $('.col-xs-4', obj).text().trim();
                 chapters.push(createChapter({
-                    id: id,
+                    id,
                     chapNum: chapNum,
-                    name: name,
-                    mangaId: mangaId,
+                    name,
+                    mangaId: mangaID,
                     langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
                     time: this.convertTime(BaotangtruyentranhParser_1.decodeHTMLEntity(time))
                 }));
